f9493d4909480eb1e37f4631335cb842a341f3a3
KozlovVP <114473389+KozlovVP@users.noreply.github.com>
2023-07-11 10:00:42 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,1 +1,10 @@
-// место для вашего кода
+// напишите решение с нуля
+// код сохраните в свой git-репозиторий
+
+#include "input_reader.h"
+#include <sstream>
+#include <iostream>
+
+int main() {
+    Read(std::cin); 
+}// место для вашего кода
