5073e83451f4953c1e18ba2104c51eafe0446ccb
KozlovVP <114473389+KozlovVP@users.noreply.github.com>
2023-07-11 10:00:04 +0300

    Update input_reader.h

--- transport-catalogue/input_reader.h
+++ transport-catalogue/input_reader.h
@@ -1,1 +1,24 @@
-// место для вашего кода
+#pragma once
+// напишите решение с нуля
+// код сохраните в свой git-репозиторий
+#include <string>
+#include <deque>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <iterator>
+#include <map>
+#include <utility>
+
+#include "transport_catalogue.h"
+
+using dis_container = std::map<std::string, std::deque<std::pair<double, std::string>>>;
+
+void OrderAddBusLess(TransportCatalogue& catalogue, std::string& command);
+void OrderAddBusDash(TransportCatalogue& catalogue, std::string& command);
+void OrderAddStop(TransportCatalogue& catalogue, std::string& command, dis_container& d_deque);
+
+void OrderGetBusInfo(TransportCatalogue& catalogue, std::string& command);
+void OrderGetStopInfo(TransportCatalogue& catalogue, std::string& command);
+
+void Read(std::istream& input);
