131ee4537d452bec6b7df14e98c9d739d3793be2
KozlovVP <114473389+KozlovVP@users.noreply.github.com>
2023-07-11 10:01:55 +0300

    Update stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -1,1 +1,17 @@
-// место для вашего кода
+#pragma once
+
+// напишите решение с нуля
+// код сохраните в свой git-репозиторий
+
+#include <string>
+#include <iostream>
+#include <set>
+
+void PrintBusInfo(std::string name, int total_size, int unique_size, double real_distance, double curv);
+
+void PrintStopInfo(std::string name, const std::set<std::string>& buses);
+
+void NotFoundBus(std::string name);
+
+void NotFoundStop(std::string name);
+
