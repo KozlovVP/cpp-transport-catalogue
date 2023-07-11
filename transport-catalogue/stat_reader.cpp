cad4b295f1ae58e6e3f5056aadfd8fb6c82cd7b0
KozlovVP <114473389+KozlovVP@users.noreply.github.com>
2023-07-11 10:01:15 +0300

    Update stat_reader.cpp

--- transport-catalogue/stat_reader.cpp
+++ transport-catalogue/stat_reader.cpp
@@ -1,1 +1,28 @@
-// место для вашего кода
+#include "stat_reader.h"
+
+using namespace std;
+
+void PrintBusInfo(string name, int total_size, int unique_size, double real_distance, double curv) {
+    cout << "Bus " << name << ": " << total_size << " stops on route, " << unique_size << " unique stops, " << real_distance << " route length, " << curv << " curvature" << endl;
+}
+
+void PrintStopInfo(std::string name, const std::set<std::string>& buses) {
+    if (buses.size() == 0) {
+        cout << "Stop " << name << ": no buses" << endl;
+        return;
+    }
+
+    cout << "Stop " << name << ": buses";
+    for (auto& bus : buses) {
+        cout << " " << bus;
+    }
+    cout << endl;
+}
+
+void NotFoundBus(string name) {
+    cout << "Bus " << name << ": not found" << endl;
+}
+
+void NotFoundStop(string name) {
+    cout << "Stop " << name << ": not found" << endl;
+}
