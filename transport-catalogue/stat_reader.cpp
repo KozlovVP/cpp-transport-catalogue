09a3ad08c35a340a322b725603575f08798b607c
KozlovVP <114473389+KozlovVP@users.noreply.github.com>
2023-07-13 17:14:03 +0300

    Update stat_reader.cpp

--- transport-catalogue/stat_reader.cpp
+++ transport-catalogue/stat_reader.cpp
@@ -2,27 +2,42 @@
 
 using namespace std;
 
-void PrintBusInfo(string& name, int total_size, int unique_size, double real_distance, double curv) {
-    cout << "Bus " << name << ": " << total_size << " stops on route, " << unique_size << " unique stops, " << real_distance << " route length, " << curv << " curvature" << endl;
+void PrintBusInfo(const string& name, int total_size, int unique_size, double real_distance, double curv) {
+    ostringstream out;
+    out << "Bus " << name << ": " << total_size << " stops on route, " << unique_size << " unique stops, " << real_distance << " route length, " << curv << " curvature" << endl;
+    string b = move(out).str();
+    cout << b;
 }
 
-void PrintStopInfo(string& name, const set<string>& buses) {
+void PrintStopInfo(const string& name, const set<string>& buses) {
+    ostringstream out;
     if (buses.size() == 0) {
-        cout << "Stop " << name << ": no buses" << endl;
+        out << "Stop " << name << ": no buses" << endl;
+        string b = move(out).str();
+        cout << b;
         return;
     }
 
-    cout << "Stop " << name << ": buses";
+    out << "Stop " << name << ": buses";
     for (auto& bus : buses) {
-        cout << " " << bus;
+        out << " " << bus;
     }
-    cout << endl;
+    out << endl;
+
+    string b = move(out).str();
+    cout << b;
 }
 
-void NotFoundBus(string name) {
-    cout << "Bus " << name << ": not found" << endl;
+void NotFoundBus(const string& name) {
+    ostringstream out;
+    out << "Bus " << name << ": not found" << endl;
+    string b = move(out).str();
+    cout << b;
 }
 
-void NotFoundStop(string name) {
-    cout << "Stop " << name << ": not found" << endl;
+void NotFoundStop(const string& name) {
+    ostringstream out;
+    out << "Stop " << name << ": not found" << endl;
+    string b = move(out).str();
+    cout << b;
 }
