9c99937ff8cb6c463ff260ee02f62cd197929656
KozlovVP <114473389+KozlovVP@users.noreply.github.com>
2023-07-17 08:26:22 +0300

    Update stat_reader.cpp

--- transport-catalogue/stat_reader.cpp
+++ transport-catalogue/stat_reader.cpp
@@ -2,19 +2,19 @@
 
 using namespace std;
 
-void PrintBusInfo(const string& name, int total_size, int unique_size, double real_distance, double curv) {
-    ostringstream out;
+void PrintBusInfo(ostream& out, const string& name, int total_size, int unique_size, double real_distance, double curv) {
+    //ostringstream out;
     out << "Bus " << name << ": " << total_size << " stops on route, " << unique_size << " unique stops, " << real_distance << " route length, " << curv << " curvature" << endl;
-    string b = move(out).str();
-    cout << b;
+    //string b = move(out).str();
+    //cout << b;
 }
 
-void PrintStopInfo(const string& name, const set<string>& buses) {
-    ostringstream out;
+void PrintStopInfo(ostream& out, const string& name, const set<string>& buses) {
+    //ostringstream out;
     if (buses.size() == 0) {
         out << "Stop " << name << ": no buses" << endl;
-        string b = move(out).str();
-        cout << b;
+        //string b = move(out).str();
+        //cout << b;
         return;
     }
 
@@ -24,20 +24,20 @@ void PrintStopInfo(const string& name, const set<string>& buses) {
     }
     out << endl;
 
-    string b = move(out).str();
-    cout << b;
+    //string b = move(out).str();
+    //cout << b;
 }
 
-void NotFoundBus(const string& name) {
-    ostringstream out;
+void NotFoundBus(ostream& out, const string& name) {
+    //ostringstream out;
     out << "Bus " << name << ": not found" << endl;
-    string b = move(out).str();
-    cout << b;
+    //string b = move(out).str();
+    //cout << b;
 }
 
-void NotFoundStop(const string& name) {
-    ostringstream out;
+void NotFoundStop(ostream& out, const string& name) {
+    //ostringstream out;
     out << "Stop " << name << ": not found" << endl;
-    string b = move(out).str();
-    cout << b;
+    //string b = move(out).str();
+    //cout << b;
 }
