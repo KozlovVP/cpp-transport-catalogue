525c364d9007223c2ded872c4e1c43a043f0cbfb
KozlovVP <114473389+KozlovVP@users.noreply.github.com>
2023-07-23 13:56:38 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,10 +1,22 @@
-// напишите решение с нуля
-// код сохраните в свой git-репозиторий
-
-#include "input_reader.h"
-#include <sstream>
 #include <iostream>
+#include <string>
+#include <string_view>
+#include <vector>
+
+#include "json_reader.h"
+#include "map_renderer.h"
+#include "request_handler.h"
+#include "transport_catalogue.h"
+
+using namespace std;
+using namespace catalogue;
 
 int main() {
-    Read(std::cin); 
-}// место для вашего кода
+    RenderSettings render_settings;
+    TransportCatalogue catalogue;
+    JsonReader json_reader(catalogue, render_settings, cin);
+    json_reader.Parse();
+    MapRenderer map_renderer(catalogue, render_settings);
+    json_reader.Handle(cout);
+    return 0;
+}
