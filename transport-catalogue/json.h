58a00b6d0167e65be1ed25f4c1b0ceae391065c3
KozlovVP <114473389+KozlovVP@users.noreply.github.com>
2023-07-23 13:55:07 +0300

    Update json.h

--- transport-catalogue/json.h
+++ transport-catalogue/json.h
@@ -1,1 +1,73 @@
+#pragma once
+
+#include <iostream>
+#include <map>
+#include <string>
+#include <variant>
+#include <vector>
+
+namespace json {
+
+class Node;
+// Сохраните объявления Dict и Array без изменения
+using Dict = std::map<std::string, Node>;
+using Array = std::vector<Node>;
+using NodeData = std::variant<std::nullptr_t, Array, Dict, bool, int, double, std::string>;
+
+// Эта ошибка должна выбрасываться при ошибках парсинга JSON
+class ParsingError : public std::runtime_error {
+public:
+    using runtime_error::runtime_error;
+};
+
+class Node {
+public:
+   /* Реализуйте Node, используя std::variant */
+    Node() {}
+    Node(std::nullptr_t) {}
+    template<typename T>
+    Node(T data) : data_(std::move(data)) {}
+
+    bool IsNull() const;
+    bool IsInt() const;
+    bool IsBool() const;
+    bool IsDouble() const;
+    bool IsPureDouble() const;
+    bool IsString() const;
+    bool IsArray() const;
+    bool IsMap() const;
+    size_t GetIndex() const;
+    const NodeData& GetData() const;
+
+    const Array& AsArray() const;
+    const Dict& AsMap() const;
+    bool AsBool() const;
+    int AsInt() const;
+    double AsDouble() const;
+    const std::string& AsString() const;
+
+    bool operator==(const json::Node& other) const;
+    bool operator!=(const json::Node& other) const;
+private:
+    NodeData data_;
+};
+
+class Document {
+public:
+    explicit Document(Node root);
+
+    const Node& GetRoot() const;
+
+    bool operator==(const Document& other) const;
+    bool operator!=(const Document& other) const;
+
+private:
+    Node root_;
+};
+
+Document Load(std::istream& input);
+
+void Print(const Document& doc, std::ostream& output);
+
+}  // namespace json
 
