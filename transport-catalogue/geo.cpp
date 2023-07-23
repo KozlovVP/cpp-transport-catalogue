0855a6e0746b35c264253e01e07ea40fb2ad8dae
KozlovVP <114473389+KozlovVP@users.noreply.github.com>
2023-07-23 13:54:48 +0300

    Update geo.cpp

--- transport-catalogue/geo.cpp
+++ transport-catalogue/geo.cpp
@@ -1,1 +1,10 @@
+#include "geo.h"
+
+namespace geo {
+    inline const double EPSILON = 1e-6;
+
+    bool IsZero(double value) {
+        return std::abs(value) < EPSILON;
+    }
+} // namespace geo
 
