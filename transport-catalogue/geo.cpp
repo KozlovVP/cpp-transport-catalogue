8675d057d743cf8740d59ab50735b2a49c2948a3
KozlovVP <114473389+KozlovVP@users.noreply.github.com>
2023-07-25 12:28:52 +0300

    Update geo.cpp

--- transport-catalogue/geo.cpp
+++ transport-catalogue/geo.cpp
@@ -1,10 +1,20 @@
+#define _USE_MATH_DEFINES
 #include "geo.h"
 
-namespace geo {
-    inline const double EPSILON = 1e-6;
+#include <cmath>
+        
+        
+        namespace geo {
+            double ComputeDistance(Coordinates start, Coordinates end) {
+                using namespace std;
 
-    bool IsZero(double value) {
-        return std::abs(value) < EPSILON;
-    }
-} // namespace geo
-
+                if (!(start == end)) {
+                    const double dr = PI / 180.;
+                    return acos(sin(start.latitude * dr) * sin(end.latitude * dr)
+                                + cos(start.latitude * dr) * cos(end.latitude * dr)
+                                  * cos(abs(start.longitude - end.longitude) * dr)) * EARTH_RADIUS;
+                } else {
+                    return 0.0;
+                }
+            }
+        }
