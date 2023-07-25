d693fdf48912cbd6a0607e4e57a24bfb5d02d166
KozlovVP <114473389+KozlovVP@users.noreply.github.com>
2023-07-25 12:32:06 +0300

    Update transport_catalogue.h

--- transport-catalogue/transport_catalogue.h
+++ transport-catalogue/transport_catalogue.h
@@ -1,47 +1,110 @@
 #pragma once
-
-#include "domain.h"
-
 #include <deque>
-#include <set>
 #include <string>
 #include <vector>
+#include <iomanip>
+#include <iostream>
+#include <unordered_set>
 #include <unordered_map>
-#include <utility>
 
-namespace catalogue {
+#include "geo.h"
+
+namespace transport_catalogue {
+
+    struct Stop {
+        std::string name_;
+        geo::Coordinates coordinates_;
+
+        bool operator==(const Stop& other) const {
+            return name_ == other.name_ && coordinates_ == other.coordinates_;
+        }
+    };
+
+    struct Bus {
+        std::string name_;
+        std::vector<const Stop *> stops_;
+        bool circular_route{};
+
+        bool operator==(const Bus& other) const {
+            return name_ == other.name_ && stops_ == other.stops_;
+        }
+        [[maybe_unused]]  bool CircularRoute() const {
+            Bus bus;
+            if (stops_.size() < 2) {
+                return false;
+            }
+            const Stop *first_stop = stops_.front();
+            const Stop *last_stop = stops_.back();
+            return first_stop == last_stop;
+        }
+    };
+
+    struct BusInfo {
+        size_t stops_on_route;
+        size_t unique_stops;
+        double route_length;
+        double curvature;
+    };
+
+    struct PairHash {
+        size_t operator()(const std::pair<const Stop*, const Stop*>& key) const {
+            size_t h1 = std::hash<const Stop*>()(key.first);
+            size_t h2 = std::hash<const Stop*>()(key.second);
+            return h1 ^ (h2 << 1);
+        }
+    };
+
+    struct StopHash {
+        size_t operator()(const Stop& stop) const {
+            return std::hash<std::string>()(stop.name_);
+        }
+    };
+
+    struct BusHash {
+        std::size_t operator()(const Bus& bus) const {
+            std::size_t result = std::hash<std::string>{}(bus.name_);
+            for (const auto* stop : bus.stops_) {
+                result ^= std::hash<std::string>{}(stop->name_);
+            }
+            return result;
+        }
+    };
+
+    using StopMap = std::unordered_map<std::string_view, Stop *>;
+    using StopToBusesMap = std::unordered_map<const Stop*, std::vector<const Bus*>>;
+    using BusMap = std::unordered_map<std::string_view, Bus *>;
+    using DistanceMap = std::unordered_map<std::pair<const Stop*, const Stop*>, int, PairHash>;
+
     class TransportCatalogue {
     public:
-        TransportCatalogue() {};
+        void AddBus(const Bus& bus);
+        void AddStop(const Stop& stop);
+        void SetDistance(const Stop& stop_from, const Stop& stop_to, int distance);
 
-        void AddStop(const std::string &name, const geo::Coordinates& coordinates);
-        void AddDistance(const std::string& from, const std::string& to, int distance);
-        void AddBus(const std::string& bus_name, const std::vector<std::string>& stops_name, bool is_roundtrip);
-        BusInfo GetBusInfo(std::string_view bus_name) const;
-        StopInfo GetStopInfo(std::string_view stop_name) const;
-        const std::unordered_map<std::string_view, Bus*> GetBusesMap() const;
-        const std::unordered_map<std::string_view, Stop*> GetStopsMap() const;
+        BusInfo GetBusInfo(const Bus& bus) const;
+
+        const Bus *GetBus(std::string_view _bus_name);
+        const Stop *GetStop(std::string_view _stop_name);
+
+        BusMap ReturnBusMap() const;
+        StopToBusesMap ReturnStopToBuses () const;
+
+        std::unordered_set<Bus, BusHash> StopGetUniqueBuses(Stop *stop) const;
+        size_t GetDistanceStop(const Stop *_start, const Stop *_finish) const;
+
+
     private:
-        class DistanceHasher {
-        public:
-            size_t operator()(std::pair<Stop*, Stop*> stops_pair) const {
-                size_t first = hasher(stops_pair.first);
-                size_t second = hasher(stops_pair.second);
-                return first + 37 * second;
-            }
-            std::hash<const void*> hasher;
-        };
+        void AddBusToStop(const Bus& bus, const Stop& stop);
+        const std::vector<const Bus*>& GetBusesForStop(const Stop* stop) const;
+        static std::unordered_set<Stop, StopHash> GetUniqueStops(const Bus *bus);
+        size_t GetDistanceToBus(const Bus *_bus) const;
+        double GetLength(const Bus *bus) const;
 
-        int ComputeStopCount(std::string_view bus_name) const;
-        int ComputeUniqueStopCount(std::string_view bus_name) const;
-        RoadInfo ComputeRoadInfo(std::string_view bus_name) const;
-        int GetRouteLength(const Stop* from, const Stop* to) const;
-
-        std::unordered_map<std::string_view, Bus*> buses_map_;
-        std::unordered_map<std::string_view, Stop*> stops_map_;
-        std::deque<Stop> stops_;
         std::deque<Bus> buses_;
-        std::unordered_map<std::string_view, std::set<std::string_view>> stop_to_buses_;
-        std::unordered_map<std::pair<Stop*, Stop*>, int, DistanceHasher> stops_to_distance_;
+        std::deque<Stop> stops_;
+        DistanceMap distances_;
+        StopMap stop_name_;
+        BusMap bus_name_;
+        StopToBusesMap stop_buses_;
     };
-} // namespace catalogue
+}
