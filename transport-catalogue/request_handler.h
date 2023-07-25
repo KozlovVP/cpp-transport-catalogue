cb2918c70e45e7e2e87dab49b9c14b005c21b7f2
KozlovVP <114473389+KozlovVP@users.noreply.github.com>
2023-07-25 12:31:00 +0300

    Update request_handler.h

--- transport-catalogue/request_handler.h
+++ transport-catalogue/request_handler.h
@@ -1,39 +1,30 @@
 #pragma once
+#include <sstream>
+#include "json_reader.h"
 
-#include <vector>
 
-#include "transport_catalogue.h"
-#include "svg.h"
+using namespace transport_catalogue;
+using namespace map_renderer;
+using namespace transport_catalogue::detail::json;
 
-namespace catalogue {
-    struct RenderSettings {
-        double width;
-        double height;
-        double padding;
-        double line_width;
-        double stop_radius;
-        int bus_label_font_size;
-        std::pair<double, double> bus_label_offset;
-        int stop_label_font_size;
-        std::pair<double, double> stop_label_offset;
-        svg::Color underlayer_color;
-        double underlayer_width;
-        std::vector<svg::Color> color_palette;
-    };
 
-    class MapRenderer {
+namespace request_handler {
+
+    class RequestHandler {
     public:
-        MapRenderer(const TransportCatalogue& catalogue, const RenderSettings& render_settings)
-                : catalogue_(catalogue)
-                , render_settings_(render_settings) {};
-        void DrawMap(svg::Document& doc);
+
+        RequestHandler() = default;
+
+        static std::vector<geo::Coordinates> GetStopsCoordinates(TransportCatalogue& catalogue_) ;
+        static std::vector<std::string_view> GetSortBusesNames(TransportCatalogue& catalogue_) ;
+
+        void ExecuteQueries(TransportCatalogue& catalogue, std::vector<StatRequest>& stat_requests, RenderSettings& render_settings);
+        static void ExecuteRenderMap(MapRenderer& map_catalogue, TransportCatalogue& catalogue_) ;
+
+        const Document& GetDocument();
+
     private:
-        void AddLines(svg::Document& doc, geo::SphereProjector& sphere_projector, std::vector<Bus*>& buses);
-        void AddRoute(svg::Document& doc, geo::SphereProjector& sphere_projector, std::vector<Bus*>& buses);
-        void AddStops(svg::Document& doc, geo::SphereProjector& sphere_projector, std::vector<Stop*>& stops);
-
-        const TransportCatalogue& catalogue_;
-        const RenderSettings& render_settings_;
+        Document doc_out;
     };
-} // namespace catalogue
 
+}
