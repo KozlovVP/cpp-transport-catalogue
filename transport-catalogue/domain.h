9b995dc05e9a9244d819b7e7f3a657b586e72e12
KozlovVP <114473389+KozlovVP@users.noreply.github.com>
2023-08-29 13:22:06 +0300

    Update domain.h

--- transport-catalogue/domain.h
+++ transport-catalogue/domain.h
@@ -1,29 +1,66 @@
 #pragma once
-#include "transport_catalogue.h"
-#include <algorithm>
+
+#include "geo.h"
+
+#include <set>
+#include <string>
 #include <vector>
-#include <ostream>
 
-    namespace domain {
+namespace catalogue {
+    struct Stop {
+        std::string name;
+        geo::Coordinates coordinates;
+        Stop(const std::string &name, const geo::Coordinates &coordinates) : name(name), coordinates(coordinates) {};
+    };
+    struct Bus {
+        std::string name;
+        std::vector<Stop*> stops;
+        bool is_roundtrip;
+        Bus(const std::string &name, const std::vector<Stop*> &stops, bool is_roundtrip) :
+                name(name), stops(stops), is_roundtrip(is_roundtrip) {};
+    };
 
-        struct StatRequest {
-            int id;
-            std::string type_;
-            std::string name_;
-        };
+    struct RoadInfo {
+        RoadInfo() = default;
 
-        struct BusInfoAfterQuery{
-            std::string_view name_;
-            bool not_found_;
-            int stops_on_route_;
-            int unique_stops_;
-            int route_length_;
-            double curvature_;
-        };
+        RoadInfo(double distance, int length)
+            : distance(distance)
+            , length(length) {
+        }
 
-        struct StopInfoAfterQuery{
-            std::string_view name_;
-            bool not_found_;
-            std::vector <std::string> buses_name_;
-        };
-    }
+        double distance = 0.0;
+        int length = 0;
+    };
+
+    struct BusInfo {
+        BusInfo() = default;
+
+        BusInfo(const std::string &bus, int stops, int unique_stops, const RoadInfo &road_info)
+            : bus(bus)
+            , stops(stops)
+            , unique_stops(unique_stops)
+            , road_info(road_info) {
+        }
+
+        std::string bus;
+        int stops = 0;
+        int unique_stops = 0;
+        RoadInfo road_info = {0.0, 0};
+    };
+
+    struct StopInfo {
+        StopInfo() = default;
+
+        StopInfo(const std::string &stop, const std::set<std::string_view>* &buses, bool stop_exist, bool buses_exist)
+            : stop(stop)
+            , buses(buses)
+            , stop_exist(stop_exist)
+            , buses_exist(buses_exist) {
+        }
+
+        std::string stop;
+        const std::set<std::string_view>* buses = nullptr;
+        bool stop_exist = false;
+        bool buses_exist = false;
+    };
+}
