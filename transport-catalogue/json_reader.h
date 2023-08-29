5a94bb96f913bc386d1afa74e3208eba7f0f6a98
KozlovVP <114473389+KozlovVP@users.noreply.github.com>
2023-08-29 13:25:05 +0300

    Update json_reader.h

--- transport-catalogue/json_reader.h
+++ transport-catalogue/json_reader.h
@@ -1,60 +1,32 @@
 #pragma once
-#include "json.h"
-#include "transport_catalogue.h"
-#include "domain.h"
-#include "map_renderer.h"
-#include "request_handler.h"
+
+#include <cstdlib>
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include "geo.h"
 #include "json_builder.h"
+#include "map_renderer.h"
+#include "transport_catalogue.h"
+#include "transport_router.h"
 
-using namespace map_renderer;
-using namespace transport_catalogue;
-using namespace transport_catalogue::detail::json::builder;
-namespace transport_catalogue::detail::json {
+namespace catalogue {
+    class JsonReader {
+    public:
+        JsonReader(TransportCatalogue& catalogue, RenderSettings& render_settings, RoutingSettings& routing_settings, std::istream& input) :
+                catalogue_(catalogue), render_settings_(render_settings), routing_settings_(routing_settings), doc_(json::Load(input)) {};
+        void Parse();
+        const json::Node& GetRequestNode() const;
+        const TransportCatalogue& GetCatalogue() const;
+        const RenderSettings& GetRenderSettings() const;
+        void Handle(std::ostream &out);
+    private:
+        TransportCatalogue& catalogue_;
+        RenderSettings& render_settings_;
+        RoutingSettings& routing_settings_;
+        std::unique_ptr<TransportRouter> router_;
+        json::Document doc_;
+    };
 
-            class JSONReader {
-            public:
-                JSONReader() = default;
-
-                JSONReader(Document doc);
-
-                JSONReader(std::istream &input);
-
-                void ParseNodeBase(const Node &root, TransportCatalogue &catalogue);
-
-                static void ParseNodeStat(const Node &root, std::vector<StatRequest> &stat_request);
-
-                static void ParseNodeRender(const Node &node, map_renderer::RenderSettings &render_settings);
-
-                void ParseNode(const Node &root, TransportCatalogue &catalogue, std::vector<StatRequest> &stat_request,
-                               map_renderer::RenderSettings &render_settings);
-
-                void Parse(TransportCatalogue &catalogue, std::vector<StatRequest> &stat_request,
-                           map_renderer::RenderSettings &render_settings);
-
-                static Stop ParseNodeStop(Node &node);
-
-                static Bus ParseNodeBus(Node &node, TransportCatalogue &catalogue);
-
-                using DistanceTuple = std::tuple<const Stop *, const Stop *, int>;
-
-                static std::vector<DistanceTuple> ParseNodeDistances(Node &node, TransportCatalogue &catalogue);
-
-                static Node ExecuteMakeNodeMap(int id_request, TransportCatalogue& catalogue, RenderSettings render_settings);
-
-                static Node ExecuteMakeNodeStop(int id_request, const StopInfoAfterQuery &query_result);
-
-                static Node ExecuteMakeNodeBus(int id_request, BusInfoAfterQuery query_result);
-
-                static BusInfoAfterQuery BusQuery(TransportCatalogue& catalogue, std::string_view bus_name);
-                static StopInfoAfterQuery StopQuery(TransportCatalogue& catalogue, std::string_view stop_name);
-                
-
-                [[maybe_unused]] const Document &GetDocument() const;
-
-            private:
-                Document document_;
-
-                static svg::Color ParseColor(const Node &node);
-            };
-
-        }
+} // namespace catalogue
