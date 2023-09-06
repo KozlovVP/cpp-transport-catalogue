f8d1b126e9c96fbaeaeaa61825a80fcb0474d9b6
KozlovVP <114473389+KozlovVP@users.noreply.github.com>
2023-09-06 12:33:50 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,24 +1,54 @@
+#include <fstream>
 #include <iostream>
-#include <string>
 #include <string_view>
-#include <vector>
 
+#include "serialization.h"
 #include "json_reader.h"
-#include "map_renderer.h"
-#include "request_handler.h"
 #include "transport_catalogue.h"
-#include "transport_router.h"
 
-using namespace std;
-using namespace catalogue;
+using namespace std::literals;
+using namespace ctlg;
 
-int main() {
-    RenderSettings render_settings;
-    RoutingSettings routing_settings;
-    TransportCatalogue catalogue;
-    JsonReader json_reader(catalogue, render_settings, routing_settings, cin);
-    json_reader.Parse();
-    MapRenderer map_renderer(catalogue, render_settings);
-    json_reader.Handle(cout);
-    return 0;
+void PrintUsage(std::ostream& stream = std::cerr) {
+    stream << "Usage: transport_catalogue [make_base|process_requests]\n"sv;
 }
+
+
+
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        PrintUsage();
+        return 1;
+    }
+
+    const std::string_view mode(argv[1]);
+
+    if (mode == "make_base"sv) {
+
+        Serialization(std::cin);
+
+    } else if (mode == "process_requests"sv) {
+
+        TransportCatalogue tr;
+        RequestHandler handler(&tr);
+
+        json::Document doc = json::Load(std::cin);
+
+        MapRenderer map;
+        handler.SetRenderMap(&map);
+
+        Deserialization(doc, handler);
+
+        TransportRouter router(tr);
+        router.InitRouter();
+        handler.SetRouter(router);
+        
+
+        PrintInformation(doc, std::cout, handler);
+
+    } else {
+        PrintUsage();
+        return 1;
+    }
+}   
