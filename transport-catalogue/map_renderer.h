a1e9f58a2caf0f77319e0d4ad9e21eb96881ab2b
KozlovVP <114473389+KozlovVP@users.noreply.github.com>
2023-09-06 12:41:34 +0300

    Update map_renderer.h

--- transport-catalogue/map_renderer.h
+++ transport-catalogue/map_renderer.h
@@ -1,38 +1,123 @@
 #pragma once
-
-#include <vector>
-
-#include "transport_catalogue.h"
 #include "svg.h"
+#include "domain.h"
+#include <vector>
+#include <optional>
+#include <ostream>
+#include <algorithm>
 
-namespace catalogue {
-    struct RenderSettings {
-        double width;
-        double height;
-        double padding;
-        double line_width;
-        double stop_radius;
-        int bus_label_font_size;
-        std::pair<double, double> bus_label_offset;
-        int stop_label_font_size;
-        std::pair<double, double> stop_label_offset;
-        svg::Color underlayer_color;
-        double underlayer_width;
-        std::vector<svg::Color> color_palette;
+namespace ctlg{
+
+    inline const double EPSILON = 1e-6;
+bool IsZero(double value) ;
+
+
+struct MapRenderData{
+    double width;
+    double height;
+    double padding;
+    double line_width;
+    double stop_radius;
+    double bus_label_font_size;
+    std::pair<double, double> bus_label_offset;
+    double stop_label_font_size;
+    std::pair<double, double> stop_label_offset;
+    svg::Color underlayer_color;
+    double underlayer_width;
+    std::vector<svg::Color> color_palette;
+};
+
+class SphereProjector {
+public:
+
+SphereProjector() = default;
+
+template <typename PointInputIt>
+SphereProjector(PointInputIt points_begin, PointInputIt points_end,
+                double max_width, double max_height, double padding)
+    : padding_(padding) 
+{
+    if (points_begin == points_end) {
+        return;
+    }
+
+    // Находим точки с минимальной и максимальной долготой
+    const auto [left_it, right_it] = std::minmax_element(
+        points_begin, points_end,
+        [](auto lhs, auto rhs) { return lhs.lng < rhs.lng; });
+    min_lon_ = left_it->lng;
+    const double max_lon = right_it->lng;
+
+    // Находим точки с минимальной и максимальной широтой
+    const auto [bottom_it, top_it] = std::minmax_element(
+        points_begin, points_end,
+        [](auto lhs, auto rhs) { return lhs.lat < rhs.lat; });
+    const double min_lat = bottom_it->lat;
+    max_lat_ = top_it->lat;
+
+    // Вычисляем коэффициент масштабирования вдоль координаты x
+    std::optional<double> width_zoom;
+    if (!IsZero(max_lon - min_lon_)) {
+        width_zoom = (max_width - 2 * padding) / (max_lon - min_lon_);
+    }
+
+    // Вычисляем коэффициент масштабирования вдоль координаты y
+    std::optional<double> height_zoom;
+    if (!IsZero(max_lat_ - min_lat)) {
+        height_zoom = (max_height - 2 * padding) / (max_lat_ - min_lat);
+    }
+
+    if (width_zoom && height_zoom) {
+        // Коэффициенты масштабирования по ширине и высоте ненулевые,
+        // берём минимальный из них
+        zoom_coeff_ = std::min(*width_zoom, *height_zoom);
+    } else if (width_zoom) {
+        // Коэффициент масштабирования по ширине ненулевой, используем его
+        zoom_coeff_ = *width_zoom;
+    } else if (height_zoom) {
+        // Коэффициент масштабирования по высоте ненулевой, используем его
+        zoom_coeff_ = *height_zoom;
+    }
+}
+
+svg::Point operator()(geo::Coordinates coords) const {
+    return {
+        (coords.lng - min_lon_) * zoom_coeff_ + padding_,
+        (max_lat_ - coords.lat) * zoom_coeff_ + padding_
     };
+}
 
-    class MapRenderer {
-    public:
-        MapRenderer(const TransportCatalogue& catalogue, const RenderSettings& render_settings)
-                : catalogue_(catalogue)
-                , render_settings_(render_settings) {};
-        void DrawMap(svg::Document& doc);
+private:
+    double padding_;
+    double min_lon_ = 0;
+    double max_lat_ = 0;
+    double zoom_coeff_ = 0;
+};
+
+class MapRenderer{
+public:
+    MapRenderer(){
+
+    }
+
+
+    MapRenderData data_;
+
+
+    void DrawMap(std::ostream& out, const std::vector<const ctlg::BusRoute*>& routes);
+
+
     private:
-        void AddLines(svg::Document& doc, geo::SphereProjector& sphere_projector, std::vector<Bus*>& buses);
-        void AddRoute(svg::Document& doc, geo::SphereProjector& sphere_projector, std::vector<Bus*>& buses);
-        void AddStops(svg::Document& doc, geo::SphereProjector& sphere_projector, std::vector<Stop*>& stops);
 
-        const TransportCatalogue& catalogue_;
-        const RenderSettings& render_settings_;
-    };
-} // namespace catalogue
+    SphereProjector projector;
+
+    svg::Text CreateText(const BusRoute* route , int pos);
+
+    svg::Text CreatePodlozhka(const svg::Text& text);
+
+
+    std::vector<const BusStop*> CreateLines(const BusRoute* route, svg::Document& doc, int pos);
+
+};
+
+}
