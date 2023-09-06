92bf5566860e850dd0ff76a701c1812ace502d1f
KozlovVP <114473389+KozlovVP@users.noreply.github.com>
2023-09-06 12:38:31 +0300

    Update json_reader.h

--- transport-catalogue/json_reader.h
+++ transport-catalogue/json_reader.h
@@ -1,32 +1,32 @@
 #pragma once
-
-#include <cstdlib>
-#include <iostream>
-#include <memory>
-#include <string>
-
-#include "geo.h"
-#include "json_builder.h"
-#include "map_renderer.h"
+#include "json.h"
 #include "transport_catalogue.h"
-#include "transport_router.h"
+#include "request_handler.h"
+#include "map_renderer.h"
 
-namespace catalogue {
-    class JsonReader {
-    public:
-        JsonReader(TransportCatalogue& catalogue, RenderSettings& render_settings, RoutingSettings& routing_settings, std::istream& input) :
-                catalogue_(catalogue), render_settings_(render_settings), routing_settings_(routing_settings), doc_(json::Load(input)) {};
-        void Parse();
-        const json::Node& GetRequestNode() const;
-        const TransportCatalogue& GetCatalogue() const;
-        const RenderSettings& GetRenderSettings() const;
-        void Handle(std::ostream &out);
-    private:
-        TransportCatalogue& catalogue_;
-        RenderSettings& render_settings_;
-        RoutingSettings& routing_settings_;
-        std::unique_ptr<TransportRouter> router_;
-        json::Document doc_;
-    };
 
-} // namespace catalogue
+namespace ctlg{
+
+class JsonReader{
+public:
+
+    JsonReader() = default;
+
+    void LoadDocument(std::istream& input);
+
+    void ParseData(RequestHandler& request);
+
+    void PrintInformation(std::ostream &output, RequestHandler &request);
+
+    void SetMapRenderer(MapRenderer& render);
+
+
+
+private:
+    json::Document doc_;
+
+};
+
+void PrintInformation(const json::Document& doc, std::ostream &output, RequestHandler &request);
+
+}
