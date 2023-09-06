b5766c49310354010e7867222e74fd399ff97883
KozlovVP <114473389+KozlovVP@users.noreply.github.com>
2023-09-06 12:36:58 +0300

    Update geo.cpp

--- transport-catalogue/geo.cpp
+++ transport-catalogue/geo.cpp
@@ -1,9 +1,31 @@
 #include "geo.h"
 
-namespace geo {
-    inline const double EPSILON = 1e-6;
+using namespace geo;
 
-    bool IsZero(double value) {
-        return std::abs(value) < EPSILON;
+double geo::ComputeDistance(geo::Coordinates from, geo::Coordinates to) {
+    using namespace std;
+    if (from == to) {
+        return 0;
     }
-} // namespace geo
+    static const double dr = 3.1415926535 / 180.;
+    return acos(sin(from.lat * dr) * sin(to.lat * dr)
+                + cos(from.lat * dr) * cos(to.lat * dr) * cos(abs(from.lng - to.lng) * dr))
+        * 6371000;
+}
+
+
+bool Coordinates::operator==(const Coordinates& other) const {
+    return lat == other.lat && lng == other.lng;
+}
+
+bool Coordinates::operator!=(const Coordinates& other) const {
+    return !(*this == other);
+}
+
+bool Coordinates::operator<(const Coordinates& other) const{
+    return std::pair(lat, lng) < std::pair(other.lat, other.lng);
+}   
+
+bool Coordinates::operator>(const Coordinates& other) const{
+    return std::pair(lat, lng) > std::pair(other.lat, other.lng);
+}
