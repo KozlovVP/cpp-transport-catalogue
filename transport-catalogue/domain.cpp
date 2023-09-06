2246c383a2b4675619e838ff07c5bec5b71e1c76
KozlovVP <114473389+KozlovVP@users.noreply.github.com>
2023-09-06 12:36:22 +0300

    Update domain.cpp

--- transport-catalogue/domain.cpp
+++ transport-catalogue/domain.cpp
@@ -1,1 +1,15 @@
+#include "domain.h"
+#include <memory>
+
+ctlg::BusStop::BusStop(geo::Coordinates cord, std::string_view name)
+{
+    coord = cord;
+    this->name = name;
+}
+
+ctlg::BusStop::BusStop(geo::Coordinates cord, std::string name)
+{
+    coord = cord;
+    this->name = std::move(name);
+}
 
